9739a3b842da67fef2df3ff24ddf638c9dd91622
al2104 <al2104@hw.ac.uk>
2025-03-17 04:06:20 +0800

    made changes to environment

--- environment_setup.sh
+++ environment_setup.sh
@@ -3,20 +3,16 @@
 set -e  # Exit immediately if a command exits with a non-zero status
 set -o pipefail  # Return the exit status of the last command in the pipe that failed
 
-# Update packages
-echo "Updating packages..."
-sudo apt update
-sudo apt upgrade -y
+# Update Homebrew
+echo "Updating Homebrew..."
+brew update
+brew upgrade
 
 # Install Node.js
 echo "Installing Node.js..."
 if ! command -v node &> /dev/null
 then
-    sudo apt install -y curl
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-    source ~/.nvm/nvm.sh
-    nvm install node
-    nvm use node
+    brew install node
 else
     echo "Node.js is already installed."
 fi
@@ -25,16 +21,17 @@ fi
 if ! command -v npm &> /dev/null
 then
     echo "npm could not be found, installing..."
-    sudo apt update
-    sudo apt install -y npm
+    brew install npm
 else
     echo "npm is already installed."
 fi
 
 # Install Python
 echo "Installing Python..."
-sudo apt install -y python3 python3-pip
+brew install python3
 
-# Install Django and additional packages using apt
-echo "Installing Django and additional packages using apt..."
-sudo apt install -y python3-django python3-djangorestframework python3-djangorestframework-simplejwt python3-django-cors-headers python3-pythonnet python3-django-crontab
+# Install Django and additional packages using Homebrew
+echo "Installing Django and additional packages..."
+brew install django
+pip3 install djangorestframework djangorestframework-simplejwt django-cors-headers pythonnet django-crontab
+echo "Installation complete!"
