b209fbada8688da76fd9838b2513cada36101657
iamreallyacar <caffeineonoverdose@gmail.com>
2025-03-26 13:19:25 +0100

    help

--- environment_setup2.sh
+++ environment_setup2.sh
@@ -5,3 +5,6 @@ set -o pipefail  # Return the exit status of the last command in the pipe that f
 
 # Install Curl
 sudo apt install curl -y
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
